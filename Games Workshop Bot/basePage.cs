8de866006d5a4520667976c155c47a38e94ae446
Filip Buczynski <filipbuczynski2@gmail.com>
2023-09-17 23:58:36 -0500

    working on basepgae and page object

---
+++ Games Workshop Bot/basePage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace BasePage
+{
+    internal class basePage
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        public void NavigateTo(string url)
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+    }
+}
+    
